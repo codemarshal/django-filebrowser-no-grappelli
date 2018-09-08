814339affbda0ea7f41ce43361f4aaa8ee60281e
tanvir hasan <tanvir002700@gmail.com>
2018-09-09 03:09:56 +0600

    modify fake model for testing

--- filebrowser/models.py
+++ filebrowser/models.py
@@ -1,9 +1,10 @@
+import sys
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 
 class FileBrowser(models.Model):
     class Meta:
-        managed = False
+        managed = sys.argv[1:2] == ['test']
         verbose_name = _("FileBrowser")
         verbose_name_plural = _("FileBrowser")
